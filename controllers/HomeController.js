c10d51a0905b043ba5d5387c006e1b5a9d68b3c6
Carlos Jaime <jaime_andrek@hotmail.com>
2022-06-01 11:15:49 -0300

    nova organização

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,10 +1,9 @@
 const {Op} = require('sequelize')
 const User = require('../models/User')
-const Chat = require('../models/Chat')
 
 const userHome = async (req, res) => {
     const users = await User.findAll({ where: { id:{ [Op.ne]: req.session.user_id } } })
-  
+
     res.render('user/home',{
         users: users
     })
---
+++ public/js/chat.js
@@ -0,0 +1,40 @@
+async function sendMsg(event){
+
+    let chat_msg = document.querySelector('[chat-msg]')
+    const codigo = event.target.id
+    const chat = document.querySelector('.chat')
+
+    const rawResponse = await fetch('/msg', {
+        method: 'POST',
+        headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            codigo: codigo,
+            msg: chat_msg.value
+        })
+    });
+    
+    chat.insertAdjacentHTML('afterbegin', `<div class="chat-right"><span>${chat_msg.value}</span><div/>`)
+    chat_msg.value = ""
+}
+
+async function updateChat(){
+    const chat = document.querySelector('.chat')
+    const offset = chat.children.length
+    const codigo = document.querySelector('.chat-send').children[1].id
+
+    let msgs = await fetch(`/msgs/${offset}/${codigo}`)
+    chatMsgs = await msgs.json()
+
+    chatMsgs.msgs.forEach(msg=>{
+        console.log(msg.msg)
+        chat.insertAdjacentHTML('afterbegin', `<div ${msg.userId == chatMsgs.id ? "class=chat-right" : "class=chat-left"} ><span>${msg.msg}</span><div/>`)
+    })
+}
+
+setInterval(()=>{
+    console.log('ola')
+    updateChat()
+},1000)
