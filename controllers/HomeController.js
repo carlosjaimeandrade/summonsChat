c0591223836664ec7d470752bc5da59bc86fb6d8
Carlos Jaime <jaime_andrek@hotmail.com>
2022-06-01 12:37:29 -0300

    Ajustando pagina home

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -3,6 +3,7 @@ const User = require('../models/User')
 
 const userHome = async (req, res) => {
     const users = await User.findAll({ where: { id:{ [Op.ne]: req.session.user_id } } })
+    
 
     res.render('user/home',{
         users: users
